33cea3263dd381773ec66a3b4b8847a9abe105f9
John Labovitz <johnl@johnlabovitz.com>
2019-05-27 10:52:34 -0400

    Sort domains before writing config file.

--- lib/h2o-configurator/builder.rb
+++ lib/h2o-configurator/builder.rb
@@ -9,7 +9,7 @@ module H2OConfigurator
         'error-log' => ErrorLogFile.to_s,
         'hosts' => {},
       }
-      Path.glob(SitesDirGlob).reject { |p| p.extname == '.old' || p.extname == '.new' }.each do |site_dir|
+      Path.glob(SitesDirGlob).reject { |p| p.extname == '.old' || p.extname == '.new' }.sort.each do |site_dir|
         site = Site.new(site_dir)
         puts "%30s => %s" % [site.name, site.dir]
         config['hosts'].merge!(site.make_config)
